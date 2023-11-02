dc26fb119c681592dc3e7a1d18591e50e4e01fa4
SmasherUltimate <149662249+SmasherUltimate@users.noreply.github.com>
2023-11-02 11:21:41 +0300

    Update 3xui_install.sh

--- 3xui_install.sh
+++ 3xui_install.sh
@@ -25,7 +25,7 @@ ufw status verbose
 echo n | bash <(curl -Ls https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh)
 /usr/local/x-ui/x-ui setting -username admin -password admin -port ${UI_PORT}
 
-wget https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2.6.4_linux_amd64.deb
+wget https://github.com/caddyserver/caddy/releases/download/v2.7.5/caddy_2.7.5_linux_amd64.deb
 dpkg -i caddy_2.6.4_linux_amd64.deb
 
 
